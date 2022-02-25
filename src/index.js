b71e083fad07b8f4cf1a6c12c573240e7b47ed71
edgarmc <edgarmc100@gmail.com>
2022-02-25 12:35:05 -0600

    :sparkles: fonts loader added to webpack

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import './styles.css'
 import WebpackImage from './assets/images/site-logo.1fcab817090e78435061.svg'
 //This fucking way is possible but I didn't find the solution yet.
 // import Styles from 'style-loader!css-loader?modules!./styles.css';
+import data from './data'
+
 function component() {
+  console.log(data)
   const element = document.createElement('div');
 
  // Lodash, now imported by this script
