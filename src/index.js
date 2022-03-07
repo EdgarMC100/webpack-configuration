6765f65357a6c073a99152203fbb4dbcf6e5cc9c
edgarmc <edgarmc100@gmail.com>
2022-03-07 15:49:03 -0600

    :sparkles: Supports csv and xml files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 import _ from 'lodash'
 import './styles.css'
-import WebpackImage from './assets/images/site-logo.1fcab817090e78435061.svg'
+// import WebpackImage from './assets/images/site-logo.1fcab817090e78435061.svg'
 //This fucking way is possible but I didn't find the solution yet.
 // import Styles from 'style-loader!css-loader?modules!./styles.css';
-import Data from './data/example.json'
+import DataJson from './data/example.json'
+import Notes from './data.csv'
+
 
 
 function component() {
-  console.log(Data,1)
+  console.log(DataJson,1)
+  console.log(Notes)
   const element = document.createElement('div');
 
  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
-  //Addthe image to our existing div.
-  const myImage = new Image();
-  myImage.src = WebpackImage;
+  //Add the image to our existing <div className="hello"></div>
+  // const myImage = new Image();
+  // myImage.src = WebpackImage;
+  // element.appendChild(myImage)
   return element;
 }
 
