d2b7d728c526c488abe07a17f54196076a342cdd
edgarmc <edgarmc100@gmail.com>
2022-03-07 18:51:04 -0600

    :sparkles: Setting up HtmlWebpaclPlugin and cleaning up

--- src/index.js
+++ src/index.js
@@ -1,36 +1,13 @@
-import _ from 'lodash'
-import './styles.css'
-// import WebpackImage from './assets/images/site-logo.1fcab817090e78435061.svg'
-//This fucking way is possible but I didn't find the solution yet.
-// import Styles from 'style-loader!css-loader?modules!./styles.css';
-import DataJson from './data/example.json'
-import Notes from './data.csv'
-import toml from './data.toml'
-import yaml from './data.yaml'
-import json from './data.json5'
-
-console.log(toml.title);
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
-
+import _ from 'lodash';
+import printMe from './print.js';
 function component() {
-  console.log(DataJson,1)
-  console.log(Notes)
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
- // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  //Add the image to our existing <div className="hello"></div>
-  // const myImage = new Image();
-  // myImage.src = WebpackImage;
-  // element.appendChild(myImage)
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,23 @@
 const path = require('path');
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   //indicates which module webpack should use to begin building out its internal dependency graph
   //Webpack will figure out which other modules and libraries that entry point depends on (directly and indirectly).
   entry: {
-    main: './src/index.js'
+    index: './src/index.js',
+    print: './src/print.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    })
+  ],
   //Tells webpack where to emit the bundles it creates and how to name these files.
   //It defaults to ./ dist / main.js for the main output file and to the ./ dist folder for any other generated file.
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname,'dist'),
-  },
-  module: {
-    rules: [
-      {
-        //Identifies which file or files should be transformed.
-        test: /\.css$/i,
-        //Indicates which loader should be used to do the transforming
-        use: ['style-loader','css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader']
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader']
-      },
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        }
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser:{
-          parse: yaml.parse,
-        }
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser:{
-          parse: json5.parse
-        }
-      }
-    ],
+    clean: true,
   },
 };
