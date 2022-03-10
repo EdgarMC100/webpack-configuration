53e0a91f25d93ed5ab59188eb4915524cedbac97
edgarmc <edgarmc100@gmail.com>
2022-03-10 14:41:39 -0600

    :wrench: Enabling HMR and it modifies our main script

--- src/index.js
+++ src/index.js
@@ -15,17 +15,19 @@ function component() {
 document.body.appendChild(component());
 
 
-//This is useful when the page is not reloaded,
+//This is useful when the page is not reloaded usin webpak-hot-middleare,
 //https://github.com/webpack-contrib/webpack-hot-middleware/issues/293
 //the only bad thing is the old changes are visibles in the view
-module.hot.accept((err) => {
-  console.log("reloading self");
-  window.location.reload();
-})
+// module.hot.accept((err) => {
+//   console.log("reloading self");
+//   window.location.reload();
+// })
 
 // if (import.meta.webpackHot) {
 //   import.meta.webpackHot.accept();
 // }
+
+//This is used when we enabling Hot Module Replacement
 // if (module.hot) {
 //   module.hot.accept('./print.js', function () {
 //     console.log('Accepting the updated printMe module!');
--- webpack.config.js
+++ webpack.config.js
@@ -7,26 +7,36 @@ module.exports = {
   mode: 'development',
   //indicates which module webpack should use to begin building out its internal dependency graph
   //Webpack will figure out which other modules and libraries that entry point depends on (directly and indirectly).
-  entry: [
-    'webpack-hot-middleware/client','./src/index.js',
-  ],
+  entry: {
+    main: './src/index.js',
+    // Runtime code for hot module replacement
+    // hot:'webpack/hot/dev-server.js',
+    // client: 'webpack-dev-server/client/index.js?hot=true&live-reload=true',
+    // Runtime code for hot module replacement
+    // hot: ,
+    // Dev server client for web socket transport, hot and live reload logic
+    // client: 'webpack-dev-server/client/index.js?hot=true&live-reload=true',
+  },
 
   //Tells webpack where to emit the bundles it creates and how to name these files.
   //It defaults to ./ dist / main.js for the main output file and to the ./ dist folder for any other generated file.
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean:true,
     publicPath: '/'
   },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
+    hot: true,
+    // client: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development environment',
     }),
     new WebpackManifestPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
+    // new webpack.HotModuleReplacementPlugin(),
   ],
 };
